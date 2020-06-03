341d002eb68f84ea9c9642ce4a2ab6025a28e43f
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-03 11:18:56 -0700

    udpate command desc

--- src/commands/gs/helper/data.ts
+++ src/commands/gs/helper/data.ts
@@ -8,11 +8,11 @@ Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages("helper", "helper");
+const messages = Messages.loadMessages("helper", "data");
 
 export default class Data extends SfdxCommand {
 
-  public static description = messages.getMessage('dataCommandDescription');
+  public static description = messages.getMessage('commandDescription');
 
   protected static flagsConfig = {
     operation: flags.string({
