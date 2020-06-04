834e3fcf1f5ff85656191531cf9b0778198e69e5
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-03 20:24:57 -0700

    test execFile

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -5,8 +5,8 @@ export function doAction(command: string) {
    * need to remove this from util since its only relevant to beta packaging
    */
   let mockPoller = setInterval(() => {  
-    // console.log('spinning up new beta package...');
-  }, 3000);
+    console.log('spinning up new beta package...');
+  }, 15000);
   console.log('utils');
   exec(command, (err, stdout, stderr) => {
     if (stdout) console.log("RESPONSE \n " + stdout);
