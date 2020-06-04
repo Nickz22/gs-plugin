0f5e3d4585d0d1f8e0670fac4f4e10353fd08e5c
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-03 21:07:02 -0700

    test execFile

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -4,9 +4,6 @@ export function doAction(command: string) {
   /**
    * need to remove this from util since its only relevant to beta packaging
    */
-  let mockPoller = setInterval(() => {  
-    console.log('spinning up new beta package...');
-  }, 15000);
   exec(command, (err, stdout, stderr) => {
     if (stdout) console.log(stdout);
     if (stderr) {
@@ -19,6 +16,5 @@ export function doAction(command: string) {
         console.log('HINT: Please cd into the "src" dir of the git repo before using plugin');
       }else console.log(stderr);
     }
-    clearInterval(mockPoller);
   });
 }
