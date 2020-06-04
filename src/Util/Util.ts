6f077be56bf9cde6e277740bcb6fa365b49f4105
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-03 19:50:39 -0700

    test execFile

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -1,14 +1,14 @@
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 
 export function doAction(command: string) {
   /**
    * need to remove this from util since its only relevant to beta packaging
    */
   let mockPoller = setInterval(() => {  
-    console.log('spinning up new beta package...');
+    // console.log('spinning up new beta package...');
   }, 3000);
   console.log('utils');
-  exec(command, (err, stdout, stderr) => {
+  execFile(command, (err, stdout, stderr) => {
     if (stdout) console.log("RESPONSE \n " + stdout);
     if (stderr) {
       if (stderr.includes("OAuth")) {
