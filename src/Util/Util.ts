396da57ace6b4e91a1e72ec5128c56a2e3e798be
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-03 19:51:52 -0700

    test execFile

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -1,4 +1,4 @@
-const { execFile } = require("child_process");
+const { execSync } = require("child_process");
 
 export function doAction(command: string) {
   /**
@@ -8,7 +8,7 @@ export function doAction(command: string) {
     // console.log('spinning up new beta package...');
   }, 3000);
   console.log('utils');
-  execFile(command, (err, stdout, stderr) => {
+  execSync(command, (err, stdout, stderr) => {
     if (stdout) console.log("RESPONSE \n " + stdout);
     if (stderr) {
       if (stderr.includes("OAuth")) {
