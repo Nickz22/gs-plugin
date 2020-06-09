14b1aada8588e5c203a81507b4481ad06a632a57
Nick Zozaya <34489533+Nickz22@users.noreply.github.com>
2020-06-09 11:01:43 -0700

    add managed package creation

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -1,103 +1,23 @@
 const { exec } = require("child_process");
 const genericError : string = 'oops, looks like an error occurred while';
 const messages = {
-  packagingError : `${genericError} creating a new package version`,
-  installError : `${genericError} installing the new version in QA`,
-  packageIdPhrase : '__PACKAGEID__',
-  successNewVersion : 'successfully uploaded new package version...',
-  installingNewVersion : 'installing new version in QA ==> ',
-  successInstall : 'successfully installed beta package in QA...',
-  betaUpgradeError : 'You cannot upgrade a beta package',
-  betaUpgradeHint : 'Please uninstall Sequence QA package and try again'
-}
-
-export async function doActionWithCallback(command: string, callback: any) {
-  let success : boolean = true;
-  await exec(command, (err, stdout, stderr) => {
-    if (err){
-      success = false;
-      log("ERROR");
-      return success;
-    }
-    if (stdout){
-      if(stdout != null && stdout.length > 0){
-        log(stdout);
-        callback();
-      }else{
-        log('empty std out...');  
-      }
-    }
-  });
-  return success;
-}
-
-export function createNewVersion(command: string, callback: any){
-  exec(command, (err, stdout, stderr) => {
-    if (stderr) {
-      log(stderr);
-      log(messages.packagingError);
-      return;
-    }
-    if (err) {
-      log(err);
-      log(messages.packagingError);
-      return;
-    }
-    if (stdout){
-      if( stdout.includes(messages.packageIdPhrase) ){
-        log(messages.successNewVersion);
-        let versionId = stdout.substring(stdout.indexOf(messages.packageIdPhrase)+messages.packageIdPhrase.length);
-        log(`${messages.installingNewVersion} ${versionId}`);
-        callback(versionId);
-      }else{
-        log(stdout);
-        log('<=== stdout package upload else ===>');
-      }
-    }
-  });
-}
-
-export function doPackageInstall(command: string, callback: any){
-  exec(command, (err, stdout, stderr) => {
-    if (stderr) {
-      if( stderr.includes(messages.betaUpgradeError) ){
-        log(messages.betaUpgradeHint);
-        return;
-      }
-      log(messages.installError);
-      log(stderr);
-    }
-    if (err) {
-      log(err);
-      if( err.includes(messages.betaUpgradeError) ){
-        log(messages.betaUpgradeHint);
-        return;
-      }
-      log(messages.installError);
-      log(stderr);
-    }
-    if (stdout){
-      if( stdout.includes('Success') || stdout.includes('success') ){
-        log(messages.successInstall);
-        callback();
-      }
-    }
-  });
+  authError : "Please run sfdx force:config:set defaultusername=your_alias",
+  dirError : 'HINT: Please cd into the "src" dir of the git repo before using plugin'
 }
 
 export function doAction(command: string){
   exec(command, (err, stdout, stderr) => {
     if (stderr) {
       if (stderr.includes("OAuth")) {
-        log(
-          "Please run sfdx force:config:set defaultusername=your_alias"
-        );
+        log(messages.authError);
       }else if(stderr.includes('No such file')){
         log(stderr);
-        log('HINT: Please cd into the "src" dir of the git repo before using plugin');
-      }else log(stderr);
+        log(messages.dirError);
+      }else{
+        log(stderr);
+      } 
     }
-    if (stdout){
+    if(stdout){
       log(stdout);
     }
   });
---
+++ src/commands/gs/package/release.ts
@@ -0,0 +1,23 @@
+import { SfdxCommand } from "@salesforce/command";
+import { Messages } from "@salesforce/core";
+import { AnyJson } from "@salesforce/ts-types";
+import { doAction } from "../../../Util/Util";
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+// console.log('here');
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages("helper", "release"); 
+
+export default class Beta extends SfdxCommand {
+  public static description = messages.getMessage('commandDescription');
+
+  commands = {
+    newRelease: 'sfdx force:package1:version:create -u pkgOrg -m  --name RDNACadence --packageid 0336F000000B3Oh'
+  };
+  public async run(): Promise<AnyJson> {
+    doAction(this.commands.newRelease);
+    return "done";
+  }
+}
